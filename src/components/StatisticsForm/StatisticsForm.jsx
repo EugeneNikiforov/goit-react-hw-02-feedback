9ddbe4d5cb715d69a3e1886acc63bf85f8df4516
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-02-03 14:02:12 +0200

    check repo

---
+++ src/components/StatisticsForm/StatisticsForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function StatisticsForm(props) {
+  return (
+    <div>StatisticsForm</div>
+  )
+};
+
+StatisticsForm.propTypes = {}
+
+export default StatisticsForm;
+
+StatisticsForm.propTypes = {};
